90e6f8542cab4250c67072d8e428e12c1066f5bb
paalsteek <github@paalsteek.de>
2012-02-14 15:16:29 +0100

    added QWidget for MusicControl and improved functionality

--- controlpanel/musiccontrol.cpp
+++ controlpanel/musiccontrol.cpp
@@ -1,107 +1,124 @@
 #include "musiccontrol.h"
 
-MusicControl::MusicControl()
+#include <QtGui/QDesktopWidget>
+
+namespace Msg
 {
-}
+    MusicControl::MusicControl()
+    {
+        qDebug() << "Initializing music control!";
+        const char *card = "default";
+        const char *selem_name = "DAC";
 
-void MusicControl::setMasterVolume(long int volume)
-{
-    long min, max;
-    snd_mixer_t *handle;
-    snd_mixer_selem_id_t *sid;
-    const char *card = "default";
-    const char *selem_name = "DAC";
+        ::snd_mixer_open(&handle, 0);
+        ::snd_mixer_attach(handle, card);
+        ::snd_mixer_selem_register(handle, NULL, NULL);
+        ::snd_mixer_load(handle);
 
-    ::snd_mixer_open(&handle, 0);
-    ::snd_mixer_attach(handle, card);
-    ::snd_mixer_selem_register(handle, NULL, NULL);
-    ::snd_mixer_load(handle);
+        snd_mixer_selem_id_alloca(&sid);
+        snd_mixer_selem_id_set_index(sid, 0);
+        snd_mixer_selem_id_set_name(sid, selem_name);
+        elem = snd_mixer_find_selem(handle, sid);
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+        snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
 
-    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-    snd_mixer_selem_set_playback_volume_all(elem, volume * max / 100);
+        vol = new VolumeWidget(min, max);
+        QPalette p(vol->palette());
+        p.setColor(QPalette::Background, Qt::darkGray);
+        vol->setPalette(p);
+        vol->setVolume(getMasterVolume());
+        vol->move(60, 180);
+    }
 
-    snd_mixer_close(handle);
-}
+    MusicControl::~MusicControl()
+    {
+        snd_mixer_close(handle);
+    }
 
-void MusicControl::increaseMasterVolume()
-{
-    long min, max;
-    snd_mixer_t *handle;
-    snd_mixer_selem_id_t *sid;
-    const char *card = "default";
-    const char *selem_name = "DAC";
+    void MusicControl::setMasterVolume(long int volume)
+    {
+        setMasterMute(1);
 
-    ::snd_mixer_open(&handle, 0);
-    ::snd_mixer_attach(handle, card);
-    ::snd_mixer_selem_register(handle, NULL, NULL);
-    ::snd_mixer_load(handle);
+        snd_mixer_selem_set_playback_volume_all(elem, volume * max / 100);
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+        vol->setVolume(getMasterVolume());
+        vol->showWidget();
+    }
 
-    long int volume;
-    snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &volume);
-    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-    qDebug() << min << " <= " << volume << " <= " << max;
-    if ( volume <= max - 3 )
-        snd_mixer_selem_set_playback_volume_all(elem, volume + 3 );
+    long int MusicControl::getMasterVolume()
+    {
+        long int volume;
+        snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &volume);
 
-    snd_mixer_close(handle);
-}
+        return volume;
+    }
 
-void MusicControl::lowerMasterVolume()
-{
-    long min, max;
-    snd_mixer_t *handle;
-    snd_mixer_selem_id_t *sid;
-    const char *card = "default";
-    const char *selem_name = "DAC";
+    void MusicControl::increaseMasterVolume()
+    {
+        setMasterMute(1);
 
-    ::snd_mixer_open(&handle, 0);
-    ::snd_mixer_attach(handle, card);
-    ::snd_mixer_selem_register(handle, NULL, NULL);
-    ::snd_mixer_load(handle);
+        long int volume = getMasterVolume();
+        qDebug() << min << " <= " << volume << " <= " << max;
+        if ( volume <= max - 3 )
+        {
+            volume += 3;
+            snd_mixer_selem_set_playback_volume_all(elem, volume);
+        }
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+        vol->setVolume(volume);
+        vol->showWidget();
+    }
 
-    long int volume;
-    snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &volume);
-    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-    qDebug() << min << " <= " << volume << " <= " << max;
-    if ( volume >= min + 3 )
-        snd_mixer_selem_set_playback_volume_all(elem, volume - 3);
+    void MusicControl::lowerMasterVolume()
+    {
+        setMasterMute(1);
+        long int volume = getMasterVolume();
+        qDebug() << min << " <= " << volume << " <= " << max;
+        if ( volume >= min + 3 )
+        {
+            volume -= 3;
+            snd_mixer_selem_set_playback_volume_all(elem, volume);
+        }
 
-    snd_mixer_close(handle);
-}
+        vol->setVolume(volume);
+        vol->showWidget();
+    }
 
-void MusicControl::setMasterMute(int value)
-{
-    snd_mixer_t *handle;
-    snd_mixer_selem_id_t *sid;
-    const char *card = "default";
-    const char *selem_name = "DAC Playback Switch";
+    //set mute flag ( 0: muted, 1: not muted )
+    void MusicControl::setMasterMute(int value)
+    {
+        snd_mixer_selem_set_playback_switch_all(elem, value);
+    }
 
-    ::snd_mixer_open(&handle, 0);
-    ::snd_mixer_attach(handle, card);
-    ::snd_mixer_selem_register(handle, NULL, NULL);
-    ::snd_mixer_load(handle);
+    VolumeWidget::VolumeWidget(int min = 0, int max = 100)
+        :QWidget(NULL, Qt::ToolTip)
+    {
+        layout = new QHBoxLayout(this);
+        QLabel* low = new QLabel();
+        low->setPixmap(QIcon(":/icon/resources/vol-down.png").pixmap(15));
+        layout->addWidget(low);
+        qDebug() << "Building Progressbar";
+        bar = new QProgressBar();
+        bar->setMaximum(max);
+        bar->setMinimum(min);
+        layout->addWidget(bar);
+        QLabel* high = new QLabel();
+        high->setPixmap(QIcon(":/icon/resources/vol-up.png").pixmap(15));
+        layout->addWidget(high);
+        timer = new QTimer();
+        timer->setInterval(1000);
+        connect(timer, SIGNAL(timeout()), this, SLOT(hide()));
+    }
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+    void VolumeWidget::setVolume(long int vol)
+    {
+        this->volume = vol;
+        bar->setValue(this->volume);
+    }
 
-    snd_mixer_selem_set_playback_switch_all(elem, value);
-
-    snd_mixer_close(handle);
+    void VolumeWidget::showWidget()
+    {
+        this->show();
+        timer->start();
+    }
 }
--- controlpanel/musiccontrol.h
+++ controlpanel/musiccontrol.h
@@ -2,17 +2,49 @@
 #define MUSICCONTROL_H
 
 #include <QtCore/QDebug>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QIcon>
+#include <QtGui/QProgressBar>
+#include <QtCore/QTimer>
 
 #include <alsa/asoundlib.h>
 
-class MusicControl
+namespace Msg
 {
-public:
-    MusicControl();
-    void setMasterVolume(long int volume);
-    void setMasterMute(int value);
-    void increaseMasterVolume();
-    void lowerMasterVolume();
-};
+    class VolumeWidget : public QWidget
+    {
+    public:
+        VolumeWidget(int min, int max);
+        void setVolume(long int vol);
+        void showWidget();
+
+    private:
+        long int volume;
+        QHBoxLayout* layout;
+        QProgressBar* bar;
+        QTimer* timer;
+    };
+
+    class MusicControl
+    {
+    public:
+        MusicControl();
+        ~MusicControl();
+        void setMasterVolume(long int volume);
+        long int getMasterVolume();
+        void setMasterMute(int value);
+        void increaseMasterVolume();
+        void lowerMasterVolume();
+
+    private:
+        snd_mixer_t *handle;
+        snd_mixer_selem_id_t *sid;
+        snd_mixer_elem_t* elem;
+        long min, max;
+
+        VolumeWidget* vol;
+    };
+}
 
 #endif // MUSICCONTROL_H
